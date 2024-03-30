168a994d415194a15247dd10f6841b91b0f8b1e7
cordelia-sixth <80014084+cordelia-sixth@users.noreply.github.com>
2024-03-31 01:58:45 +0900

    add: フッター作成

--- src/app/_layout/Container.tsx
+++ src/app/_layout/Container.tsx
@@ -3,7 +3,7 @@
  */
 export const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mx-auto flex min-h-dvh w-full flex-col text-base tracking-wider text-white/[.8] sm:text-lg">
+    <div className="mx-auto flex min-h-dvh w-full flex-col text-base tracking-wider sm:text-lg">
       {children}
     </div>
   );
--- src/app/_layout/Footer.tsx
+++ src/app/_layout/Footer.tsx
@@ -1,13 +1,49 @@
+import Link from "next/link";
+import { CiInstagram } from "react-icons/ci";
+
 /**
  * フッターコンポーネント
  */
 export const Footer = () => {
   return (
-    <footer className="flex items-start justify-between">
-      <h1>Footer</h1>
-      <p>copyright</p>
-      <p>インスタグラム</p>
-      <p>トップへ戻る</p>
+    <footer className="flex flex-col items-center gap-8 py-5">
+      <div className="flex items-center gap-2">
+        <h1 className="p-2 py-3">SUGOI Restran</h1>
+        <Link href="#">
+          <CiInstagram size={30} />
+        </Link>
+      </div>
+
+      <table cellPadding={6}>
+        <tbody className="">
+          <tr>
+            <th className="text-left align-top">Address</th>
+            <td colSpan={2}>
+              〒123-4567
+              <br />
+              東京都中央区中央町1-2-3
+              <br />
+              中央駅より徒歩3分
+            </td>
+          </tr>
+          <tr>
+            <th className="text-right">Tel</th>
+            <td>03-1234-5678</td>
+          </tr>
+          <tr>
+            <th className="text-right">Mail</th>
+            <td>info@sugoirestrant.com</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div className="flex flex-col items-center gap-3">
+        <p>情報セキュリティ方針</p>
+        <p>個人情報方針</p>
+        <p>プライバシーポリシー</p>
+      </div>
+
+      <small>&copy; SUGOI Restran</small>
     </footer>
   );
 };

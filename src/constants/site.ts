a83bd3f9cd8f7ea6c32fc2d753fcf7684d57cfd3
cordelia-sixth <80014084+cordelia-sixth@users.noreply.github.com>
2024-04-02 23:29:43 +0900

    add: メタデータを追加

---
+++ src/constants/site.ts
@@ -0,0 +1,10 @@
+/** サイト名 */
+export const SITE_NAME = "SUGOI Restaurant";
+/** サイトURL */
+export const SITE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://demo-restaurant-beige.vercel.app"
+    : "http://localhost:3000";
+/** サイト説明 */
+export const SITE_DESCRIPTION =
+  "イタリアンレストランをイメージしたデモサイトです。";
